b95c1e5d0de79ef0baebf3f8678cf8b1d9cb2068
JorgeOviedo2005 <jorge.oviedo@epn.edu.ec>
2026-07-01 23:31:11 -0500

    Agregación de la clase del Personaje del juego

--- src/main/java/ec/edu/epn/sokoban/model/escenario/Personaje.java
+++ src/main/java/ec/edu/epn/sokoban/model/escenario/Personaje.java
@@ -1,10 +1,30 @@
 package ec.edu.epn.sokoban.model.escenario;
 
+/**
+ * El personaje del escenario es representado como una casilla no transitable.
+ */
 public class Personaje extends Casilla {
-    public Personaje(int f, int c) {
-        super(f, c);
+    /**
+     * Un personaje sin coordenadas explicitas es inicializado.
+     */
+    public Personaje() {
     }
 
+    /**
+     * Un personaje es inicializado con coordenadas dentro del escenario.
+     *
+     * @param fila fila asignada al personaje
+     * @param columna columna asignada al personaje
+     */
+    public Personaje(int fila, int columna) {
+        super(fila, columna);
+    }
+
+    /**
+     * El estado de transitabilidad es retornado como bloqueado.
+     *
+     * @return false porque el personaje bloquea el paso libre
+     */
     @Override
     public boolean esTransitable() {
         return false;
